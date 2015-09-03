f4e466d061dc4b7592a52072dcb1cc0085fe44e3
Onur Cem Celebi <onur_cem.celebi@baloise.com>
2015-09-03 11:28:01 +0200

    Add support for mask and unmask

--- lib/puppet/type/systemctl.rb
+++ lib/puppet/type/systemctl.rb
@@ -10,12 +10,11 @@ Puppet::Type.newtype(:systemctl) do
 
   newparam(:name, :namevar => :true) do
      desc "The name of the service to configure."
-     newvalues(/^([a-z0-9-]+)$/i)
   end 
 
   newproperty(:command) do
-    desc "Command to pass to systemctl. One of 'enable', 'disable', 'start', 'stop', 'restart', 'try-restart', 'reload', 'reload-or-restart', 'reload-or-try-restart'"
-    newvalues('enable', 'disable', 'start', 'stop', 'restart', 'try-restart', 'reload', 'reload-or-restart', 'reload-or-try-restart')
+    desc "Command to pass to systemctl. One of 'enable', 'disable', 'start', 'stop', 'restart', 'try-restart', 'reload', 'reload-or-restart', 'reload-or-try-restart', 'mask', 'unmask'"
+    newvalues('enable', 'disable', 'start', 'stop', 'restart', 'try-restart', 'reload', 'reload-or-restart', 'reload-or-try-restart', 'mask', 'unmask')
   end
 
 end
